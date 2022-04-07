09cebf127b7b3f86fcaac03c5079b6983dcbf3ae
alzkdpf999 <alzkdpf999@naver.com>
2022-04-07 14:59:43 +0900

    lea

--- src/scripts/searcher.java
+++ src/scripts/searcher.java
@@ -7,9 +7,13 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -24,86 +28,89 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-@SuppressWarnings({"rawtypes","unchecked","nls"})
-public class indexer {
-	private String input_file;	
-	private String output_file = "./index.post";
+@SuppressWarnings({ "rawtypes", "unchecked", "nls" })
+public class searcher {
+	private String input_file;
 
-	public indexer(String path) throws Exception {
-		this.input_file=path;
-		File files = new File(input_file);
+	public searcher(String path) {
+		this.input_file = path;
+	}
+
+	public void harddf() {
+		System.out.println("5주차실행완료!!");
+	}
+
+	public void calcsim(String Q) throws Exception {
+		HashMap<String, String> titleinput = new HashMap<>();
+		HashMap<String, Double> simword = new HashMap<>();
+		// 일단 post파일 읽고 해쉬맵에 일단 저장한뒤 title을 같이 저장해야한다.
+
+		FileInputStream filestream = new FileInputStream(input_file);
+		ObjectInputStream objectInputStream = new ObjectInputStream(filestream);
+		Object object = objectInputStream.readObject();
+		objectInputStream.close();
+		HashMap store = (HashMap) object;
+		// title뽑아오기 위해
+		File files = new File("./collection.xml");
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 		Document document = docBuilder.parse(files);
 		document.getDocumentElement().normalize();
-		NodeList nList = document.getElementsByTagName("doc");
-		HashMap<String, Integer> docfre= new HashMap<>(); // df(x)계산 
-		List hashmaplist =new ArrayList<Object>();
-		String bodyData;
-		
+		NodeList nList = document.getElementsByTagName("doc"); // 제목얻기
+
+		KeywordExtractor ke = new KeywordExtractor(); // 질의어 키워드대로 일단 나누기
+		KeywordList kl = ke.extractKeyword(Q, true);
 		for (int temp = 0; temp < nList.getLength(); temp++) {
 			Node nNode = nList.item(temp);
-			HashMap<String, Integer> wordtf = new HashMap<>(); //문서마다 단어와 빈도수 저장1
-			
 			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 				Element eElement = (Element) nNode;
-				bodyData = eElement.getElementsByTagName("body").item(0).getTextContent();
-				
-			String [] re=bodyData.split("#");
-			for(String s: re)
-			{	
-				String[] re1=s.split(":");
-				String word = re1[0];
-				int tf = Integer.parseInt(re1[1]);
-				wordtf.put(word, tf);
-				if(docfre.containsKey(word)) {
-				docfre.put(word, docfre.get(word)+1);
-				}
-				else {
-					docfre.put(word,1);
-				}
-			}
-			
-		}
-			hashmaplist.add(wordtf);
-		}
-		HashMap<String, String> call = new HashMap<>(); // 값을 저장 
-		for(String word : docfre.keySet()) 
-		{
-			int num = docfre.get(word); 
-			StringBuilder store= new StringBuilder();
-			for(int i=0; i<nList.getLength();i++)
-			{
-				
-				HashMap<String, Integer> caltfidf= (HashMap<String, Integer>) hashmaplist.get(i);
-				int caltf=caltfidf.getOrDefault(word, 0);
-				double tfidf = caltf * Math.log(nList.getLength()/num) ;
-				store.append(String.format("%d %.2f ", i, tfidf));
-			}
-			call.put(word, store.toString());
-		}
-		 FileOutputStream fileStream = new FileOutputStream(output_file);
-	        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileStream);
+				String title = eElement.getElementsByTagName("title").item(0).getTextContent();
+				String id = eElement.getAttributes().getNamedItem("id").getTextContent();
+				titleinput.put(id, title);
+				double sim = 0.0;
 
-	        objectOutputStream.writeObject(call);
-	        objectOutputStream.close();
-}
-	
-	
-	//굳이 할 필요 없는 부분
-	public void readpost() throws Exception {
-		FileInputStream filestream=new FileInputStream(output_file);
-		ObjectInputStream objectInputStream = new ObjectInputStream(filestream);
-		Object object=objectInputStream.readObject();
-		objectInputStream.close();
-		HashMap hashMap=(HashMap)object;
-		Iterator<String> it=hashMap.keySet().iterator();
-	while(it.hasNext()) {
-		String key=it.next();
-		String value = (String)hashMap.get(key);
-		System.out.println(key+":::"+value);
+				for (Keyword kw : kl) {
+					String word = kw.getString();
+					double wQ = kw.getCnt();
+					if (!store.containsKey(word)) {
+						continue;
+					}
+					HashMap weight = (HashMap) store.get(word);
+					double weightdoc = (double) weight.get(id);
+
+					double innerpro = wQ * weightdoc;
+					sim += innerpro;
+				}
+				simword.put(id, sim);
+			}
+		}
+
+		// Map.Entry 리스트 작성
+		HashMap<String, Double> show = new LinkedHashMap<>();
+		ArrayList<Entry<String, Double>> list_entries = new ArrayList<Entry<String, Double>>(simword.entrySet());
+
+		// 비교함수 Comparator를 사용하여 내림 차순으로 정렬
+		Collections.sort(list_entries, new Comparator<Entry<String, Double>>() {
+			// compare로 값을 비교
+			public int compare(Entry<String, Double> obj1, Entry<String, Double> obj2) {
+				// 내림 차순으로 정렬
+				return obj2.getValue().compareTo(obj1.getValue());
+			}
+		});
+		for (Entry<String, Double> entry : list_entries) {
+			show.put(entry.getKey(), entry.getValue());
+		}
+		ArrayList<String> print = new ArrayList(show.keySet());
+		System.out.println();
+		for (int k = 0; k < 3; k++) {
+			String key = print.get(k);
+			if (show.get(key) != 0) {
+				String title = titleinput.get(key);
+				System.out.println((k + 1) + "순위" + " " + title);
+			} else if (show.get(key) == 0) {
+				System.out.println("검색된 문서가 없습니다.");
+			}
+		}
 	}
-		
-	}
-		
+
 }
