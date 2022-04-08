8a7d24441693737619744b8d3d57470ebc5839ac
alzkdpf999 <alzkdpf999@naver.com>
2022-04-08 11:31:31 +0900

    제발 마지막

--- src/scripts/searcher.java
+++ src/scripts/searcher.java
@@ -9,11 +9,18 @@ import org.snu.ids.ha.index.KeywordList;
 @SuppressWarnings({ "rawtypes", "unchecked", "nls" })
 public class searcher {
 
-	public double InnerProduct(double wQ,double weightdoc){
-		double innerpro=wQ*weightdoc;
-		
-		
-		return innerpro;
+	public double InnerProduct(HashMap store,String docid, KeywordList kl){
+		double innerproductreturn=0.0;
+		for(Keyword kw : kl) {
+			String word= kw.getString();
+			double wQ=kw.getCnt();
+			if(!store.containsKey(word)) {
+				continue;
+			}
+			HashMap weight =(HashMap)store.get(word);
+			double weightdoc=(double)weight.get(docid);
+			innerproductreturn=wQ*weightdoc;
 		}
-	
+		return innerproductreturn;
+}
 }
